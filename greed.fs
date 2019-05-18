951029680d2ff196eda0ade7c9f8fcdb3b80faaa
Scott Hutchinson <scotthutchinson@usa.net>
2019-05-18 09:04:09 -0700

    first commit to recursive branch.

--- greed.fs
+++ greed.fs
@@ -1,7 +1,6 @@
 module Greed
 
 let score dice =
-    let tripleSize = 3
     let sortedDice = dice |> List.sort
     let calcTripleScore n =
         match n with
@@ -14,26 +13,12 @@ let score dice =
         | 5 -> 50
         | _ -> 0
 
-    (* Returns a pair of the maximum scoring triple or 0 if no triples,
-       and an index of that triple or None.
-    *)
-    let tryFindHighestScoringTriple () =
-        sortedDice
-        |> List.windowed tripleSize
-        |> List.mapi (
-            fun i w -> if w |> List.forall ((=) w.Head)
-                         then calcTripleScore w.Head, Some i else 0, None)
-        |> List.maxBy (fun (s, _) -> s)
+    let rec calc acc lst =
+        match lst with
+        | [] -> acc
+        | a :: b :: c :: t when a = b && b = c ->
+            calc (acc + calcTripleScore a) t
+        | h :: t ->
+            calc (acc + calcSingleScore h) t
 
-    let tripleScore, windowStart = tryFindHighestScoringTriple ()
-
-    let singles windowStart =
-        match windowStart with
-        | Some i ->
-            (sortedDice |> List.take i) @ 
-            (sortedDice |> List.skip (tripleSize - i) |> List.take (tripleSize - 1 - i))
-        | _ -> sortedDice
-
-    let singlesList = singles windowStart
-    let singlesScore = singlesList |> List.sumBy calcSingleScore
-    tripleScore + singlesScore
+    calc 0 sortedDice
