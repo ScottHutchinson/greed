73959be47c585d6ae11aeb50d3cdf68c5681204a
Scott Hutchinson <scotthutchinson@usa.net>
2019-05-18 08:52:00 -0700

    first commit to GroupBy branch.

--- greed.fs
+++ greed.fs
@@ -2,7 +2,7 @@ module Greed
 
 let score dice =
     let tripleSize = 3
-    let sortedDice = dice |> List.sort
+
     let calcTripleScore n =
         match n with
         | 1 -> 1000
@@ -14,26 +14,22 @@ let score dice =
         | 5 -> 50
         | _ -> 0
 
-    (* Returns a pair of the maximum scoring triple or 0 if no triples,
-       and an index of that triple or None.
-    *)
-    let tryFindHighestScoringTriple () =
-        sortedDice
-        |> List.windowed tripleSize
-        |> List.mapi (
-            fun i w -> if w |> List.forall ((=) w.Head)
-                         then calcTripleScore w.Head, Some i else 0, None)
-        |> List.maxBy (fun (s, _) -> s)
+    let groups = dice |> List.groupBy id
 
-    let tripleScore, windowStart = tryFindHighestScoringTriple ()
+    let tripleScore =
+        (0, groups)
+        ||> List.fold (fun acc (n, g) ->
+                    let len = List.length g
+                    let score = if len >= tripleSize then calcTripleScore n else 0
+                    max acc score
+            )
 
-    let singles windowStart =
-        match windowStart with
-        | Some i ->
-            (sortedDice |> List.take i) @ 
-            (sortedDice |> List.skip (tripleSize - i) |> List.take (tripleSize - 1 - i))
-        | _ -> sortedDice
+    let singleScore =
+        groups
+        |> List.sumBy (fun (n, g) ->
+                    let len = List.length g
+                    let numSingles = if len >= tripleSize then len - tripleSize else len
+                    numSingles * calcSingleScore n
+            )
 
-    let singlesList = singles windowStart
-    let singlesScore = singlesList |> List.sumBy calcSingleScore
-    tripleScore + singlesScore
+    tripleScore + singleScore
